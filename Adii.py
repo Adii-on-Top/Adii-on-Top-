7b21cd3ee509e98fd046a91bec1b72bc897020bc
Adii-on-Top <126926740+Adii-on-Top@users.noreply.github.com>
2023-03-04 19:01:55 +0500

    Update and rename README.md to Adii.py
    rm -rf Adii-on-Top 
    git clone https://github.com/Adii-on-Top
    cd Adii
    git pull
    pythonAdii.py

---
+++ Adii.py
@@ -0,0 +1,14 @@
+import os
+print('give star to our repo')
+os.system('git pull && clear')
+os.system('xdg-open https://github.com/Adii-on-Top')
+#print('update finish')
+print('Join me on Facebook')
+os.system(‘https://www.facebook.com/raja.sahil.3939503')
+
+
+os.system('clear')
+
+#print('tool is on update bro....... \nCheck our group for update -- https://m.facebook.com/groups/1247184652736578/')
+#exit()
+import XXV
